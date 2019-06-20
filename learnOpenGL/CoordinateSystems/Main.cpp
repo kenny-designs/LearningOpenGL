c28eb977db8861b4c903baa4eb62d2adf73f6386
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-06-20 15:15:38 -0400

    Finished CoordinateSystems challenges

--- learnOpenGL/CoordinateSystems/Main.cpp
+++ learnOpenGL/CoordinateSystems/Main.cpp
@@ -221,6 +221,7 @@ int main()
 		// create transformations
 		glm::mat4 view			= glm::mat4(1.0f);
 		glm::mat4 projection	= glm::mat4(1.0f);
+		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		// pass transformation matrices to the shader
@@ -235,6 +236,10 @@ int main()
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
 			float angle = 20.0f * i;
+			if (i % 3 == 0)
+			{
+				angle += glfwGetTime() * 25.0f;
+			}
 			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			ourShader.setMat4("model", model);
 
